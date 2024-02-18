a1ae916fdb74e16891ee0f3923c6d9792de90640
loay <loaymannaa@outlook.com>
2024-02-18 16:42:28 +0100

    18/2 index update

--- mental-health-app/src/index.js
+++ mental-health-app/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from "./Component/Header"
+import Footer from './Component/Footer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <Header />
+   
+    <Footer />
+
   </React.StrictMode>
 );
 
