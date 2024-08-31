6835a362fedf1cdaccf400413b831b951bd2cab4
loay <loaymannaa@outlook.com>
2024-08-31 20:04:29 +0200

    url update

--- Backend/Backend/Backend/urls.py
+++ Backend/Backend/Backend/urls.py
@@ -2,16 +2,16 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
-from mental_health_app.views import ChatRoomList, ChatMessageList
-from mental_health_app.views import WorkshopList, ResourceList,PostList ,EventList
+from mental_health_app.views import ChatRoomList, ChatMessageListView
+from mental_health_app.views import WorkshopListView, ResourceList,PostList ,EventList
 
 def homepage(request):
     return HttpResponse("Welcome to the Mental Health App!")
 
 
 urlpatterns = [
-    path('api/chat-messages/', ChatMessageList.as_view(), name='chat-message-list'),
-    path('api/workshops/', WorkshopList.as_view(), name='workshop-list'),
+    path('api/chat-messages/', ChatMessageListView.as_view(), name='chat-message-list'),
+    path('api/workshops/', WorkshopListView.as_view(), name='workshop-list'),
     path('api/resources/', ResourceList.as_view(), name='resource-list'),
     path('api/chatrooms/', ChatRoomList.as_view(), name='chatroom-list'),
     path('api/posts/', PostList.as_view(), name='post-list'),
